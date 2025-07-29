476b2683f8e4f419c036e763ce42c5c9211fc15f
Agustin Camera <114038165+Agust18@users.noreply.github.com>
2025-07-29 19:36:57 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+alert ("Bienvenido a mi sitio, aprende recetas para compartir con el mundo!");
+
+function activarModoInclusivo() {
+      document.body.classList.toggle("modo-inclusivo");
+    }
+  
+function mostrarAutor() {
+      alert("👨‍💻 Sitio creado por Agustín Camera\n📍Chacabuco, Buenos Aires\n📧 agustin@hotmail.com");
+    }
+
